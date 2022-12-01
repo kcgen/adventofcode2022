50cd9dc8c04a6b05a897f83b271a9aea98b95bd2
kcgen <kcgen@users.noreply.github.com>
2022-12-01 10:16:40 -0800

    Add day 1

---
+++ day01/process.py
@@ -0,0 +1,138 @@
+#!/bin/env python3
+
+# Copyright (C) 2022-2022  kcgen <kcgen@users.noreply.github.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""
+--- Day 1: Calorie Counting ---
+
+Santa's reindeer typically eat regular reindeer food, but they need a
+lot of magical energy to deliver presents on Christmas. For that,
+their favorite snack is a special type of star fruit that only grows
+deep in the jungle. The Elves have brought you on their annual
+expedition to the grove where the fruit grows.
+
+To supply enough magical energy, the expedition needs to retrieve a
+minimum of fifty stars by December 25th. Although the Elves assure you
+that the grove has plenty of fruit, you decide to grab any fruit you
+see along the way, just in case.
+
+Collect stars by solving puzzles. Two puzzles will be made available
+on each day in the Advent calendar; the second puzzle is unlocked when
+you complete the first. Each puzzle grants one star. Good luck!
+
+The jungle must be too overgrown and difficult to navigate in vehicles
+or access from the air; the Elves' expedition traditionally goes on
+foot. As your boats approach land, the Elves begin taking inventory of
+their supplies. One important consideration is food - in particular,
+the number of Calories each Elf is carrying (your puzzle input).
+
+The Elves take turns writing down the number of Calories contained by
+the various meals, snacks, rations, etc. that they've brought with
+them, one item per line. Each Elf separates their own inventory from
+the previous Elf's inventory (if any) by a blank line.
+
+For example, suppose the Elves finish writing their items' Calories
+and end up with the following list:
+
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+
+This list represents the Calories of the food carried by five Elves:
+
+    The first Elf is carrying food with 1000, 2000, and 3000
+    Calories, a total of 6000 Calories.
+
+    The second Elf is carrying one food item with 4000 Calories.
+
+    The third Elf is carrying food with 5000 and 6000 Calories, a
+    total of 11000 Calories.
+
+    The fourth Elf is carrying food with 7000, 8000, and 9000
+    Calories, a total of 24000 Calories.
+
+    The fifth Elf is carrying one food item with 10000 Calories.
+
+In case the Elves get hungry and need extra snacks, they need to know
+which Elf to ask: they'd like to know how many Calories are being
+carried by the Elf carrying the most Calories. In the example above,
+this is 24000 (carried by the fourth Elf).
+
+Find the Elf carrying the most Calories. How many total Calories is
+that Elf carrying?
+
+"""
+
+import argparse, pathlib
+import sys
+from dataclasses import dataclass
+
+
+@dataclass
+class Elf:
+    id: int = 0
+    total_cals: int = 0
+
+    def AddCals(self, cals):
+        self.total_cals += cals
+
+    def __lt__(self, other):
+        return self.total_cals < other.total_cals
+
+
+def get_next_elf(elf) -> Elf:
+    return Elf(id=elf.id + 1)
+
+
+def process_list(items) -> Elf:
+    elf = Elf(id=1)
+    elf_with_most = Elf(id=0)
+
+    for item in items:
+        if item.strip().isdigit():
+            elf.AddCals(int(item))
+        else:
+            elf_with_most = max(elf, elf_with_most)
+            elf = get_next_elf(elf)
+
+    return elf_with_most
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Find the Elf holding the most calories (Advent of Code: Day 1)"
+    )
+    parser.add_argument(
+        "calories_file",
+        metavar="FILE",
+        type=pathlib.Path,
+        help="A file holding zero or more calorie lists. One calorie per line."
+        "A blank lines terminate the given elf's list.",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    with args.calories_file.open("r") as items:
+        elf_with_most = process_list(items)
+        print(elf_with_most)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
